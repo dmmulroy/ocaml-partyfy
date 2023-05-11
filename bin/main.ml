e894c9b4aec1413ee0a7bda7f5eeddb9a0623d51
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-05-11 11:09:24 -0400

    init commit

---
+++ bin/main.ml
@@ -0,0 +1,20 @@
+open Stdio
+
+let write_bytes_to_file (file_path : string) (bytes : Bytes.t) : unit =
+  Out_channel.with_file file_path ~f:(fun channel ->
+      Out_channel.output_string channel @@ Bytes.to_string bytes)
+
+let read_channel_to_bytes (channel : In_channel.t) : Bytes.t option =
+  let size = Int64.to_int @@ In_channel.length channel in
+  let buf = Bytes.create size in
+  match In_channel.really_input channel ~buf ~pos:0 ~len:size with
+  | Some () -> Some buf
+  | None -> None
+
+let read_file_to_bytes (file_path : string) : Bytes.t option =
+  In_channel.with_file file_path ~f:read_channel_to_bytes
+
+let _ =
+  match read_file_to_bytes "ocaml-logo.png" with
+  | Some bytes -> write_bytes_to_file "ocaml-logo-copy.png" bytes
+  | None -> printf "Error reading file\n"
